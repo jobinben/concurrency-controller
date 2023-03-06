6b4d615360d39056352c2d9ce2c4eda9e4038186
jobinben <1021137079@qq.com>
2023-03-06 22:59:23 +0800

    用函数式实现的并发控制器，concurrencyController

---
+++ concurrency.js
@@ -0,0 +1,103 @@
+// 并发控制器
+
+// 方法1: 函数式实现
+const ConcurrencyController = function (maxConcurrency) {
+
+    let runningCount = 0;
+    let queue = [];
+
+    // 5. 执行下一个任务
+    const nextTask = () => {
+        runningCount--;
+
+        if (queue.length > 0) {
+            const task = queue.shift();
+            task();
+        }
+    }
+
+    // 4. 开始跑任务
+    const runTask = async (fn, args, resolve) => {
+        runningCount++;
+
+        const result = (async () => fn(...args))();
+        resolve(result);
+
+        try {
+            await result;
+        } catch {
+
+        }
+
+        nextTask();
+    }
+
+    // 2. 入队，等待执行
+    const enqueue = (fn, args, resolve) => {
+        queue.push(runTask.bind(null, fn, args, resolve));
+
+        (async () => {
+            // 该微任务是用来等待，所有宏任务（setTimeout）进入队列后， 再开始执行
+            await Promise.resolve();
+            // 3. 当前满足条件，可以先执行任务
+            if (runningCount < maxConcurrency && queue.length > 0) {
+                const task = queue.shift();
+                task();
+            }
+        })()
+
+    }
+
+
+    // 1. 重新返回一个Promise
+    const generator = function (fn, ...args) {
+        return new Promise((resolve) => {
+            enqueue(fn, args, resolve);
+        })
+    }
+
+    // 6. 增加一些可读属性
+    Object.defineProperties(generator, {
+        runningCount: {
+            get: () => runningCount,
+        },
+        queueSize: {
+            get: () => queue.length,
+        },
+        clearQueue: {
+            value: () => {
+                queue = [];
+            }
+        }
+    })
+
+
+    return generator;
+}
+
+// test:
+const cc = ConcurrencyController(2);
+
+(async () => {
+    const asyncFn = (v, d) => {
+        return new Promise(resolve => {
+            console.log('message: ', v, 'running: ', cc.runningCount, 'queue size: ', cc.queueSize);
+            setTimeout(() => {
+                resolve(v);
+            }, d * 1e3);
+        })
+    };
+    const asyncArr = [
+        cc(() => asyncFn('a_3', 3)),
+        cc(() => asyncFn('a_2', 2)),
+        cc(() => asyncFn('b_1', 1)),
+        cc(() => asyncFn('b_2', 2)),
+        cc(() => asyncFn('c_2', 2)),
+        cc(() => asyncFn('c_2', 3)),
+        cc(() => asyncFn('d_3', 1)),
+    ];
+
+    const res = await Promise.all(asyncArr);
+
+    console.log('res: ', res);
+})();
